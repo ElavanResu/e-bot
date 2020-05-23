69943a7d5ae0180ea813745efee4bbe2b46cd5a5
shubham <shubham.navale@coradius.io>
2020-05-24 05:03:29 +0530

    Added a qn

--- commands/qn.js
+++ commands/qn.js
@@ -1,7 +1,12 @@
 module.exports = {
 	name: 'qn',
 	description: 'Asks question to bot',
+	usage: '<question>',
 	execute(message, args) {
-		message.channel.send('Hi, I am a slave of ElavanResu.');
+		if (!args.length) return message.channel.send('Provide one question.');
+		const question = args.splice(1, args.length - 1).toString().toLowerCase().replace(/[, ]+/g, ' ');
+		if (question.length > 0 && question === 'how are you?') {
+			message.channel.send('I am fine.');
+		}
 	},
 };
