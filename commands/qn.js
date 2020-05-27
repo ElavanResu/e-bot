f17565c6ec5b788d4b84db366587a5af398d9e10
shubham <shubham.navale@coradius.io>
2020-05-27 23:53:29 +0530

    Added small question

--- commands/qn.js
+++ commands/qn.js
@@ -4,9 +4,15 @@ module.exports = {
 	usage: '<question>',
 	execute(message, args) {
 		if (!args.length) return message.channel.send('Provide one question.');
-		const question = args.splice(1, args.length - 1).toString().toLowerCase().replace(/[, ]+/g, ' ');
+		const question = args.splice(0, args.length).toString().toLowerCase().replace(/[, ]+/g, ' ');
 		if (question.length > 0 && question === 'how are you?') {
 			message.channel.send('I am fine.');
+		}
+		if (question.length > 0 && question === 'am i loosing brain cells?') {
+			message.channel.send('Yes.');
+		}
+		if (question.length > 0 && question === 'what should i do?') {
+			message.channel.send('Just fly away my master.');
 		}
 	},
 };
